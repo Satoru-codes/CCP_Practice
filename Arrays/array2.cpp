72a58bcba48a95c82b88749a24450829a0f09c5e
Satoru-codes <whotanmayai@gmail.com>
2026-06-08 16:30:40 +0530

    Practice arrays with max min sum and average

---
+++ Arrays/array2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,arr[10], max,min ,sum=0;
+    float avg;
+    cout<<"Enter the elements of the array:\n";
+    for(i=0;i<10;i++){
+        cin>>arr[i];
+    }
+    max=arr[0];
+    for(i=0;i<10;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    min=arr[0];
+    for(i=0;i<10;i++){
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    for(i=0;i<10;i++){
+        sum+=arr[i];
+    }
+    avg=(float)sum/10;
+    cout<<"Maximum:"<<max<<"\n"<<"Minimum:"<<min<<"\n"<<"Sum:"<<sum<<"\n"<<"Average:"<<avg;
+}
