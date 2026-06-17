7853261674a8dbca7c7f5d0822fcac954dd8e8ac
Satoru-codes <whotanmayai@gmail.com>
2026-06-17 21:40:53 +0530

    feat: add armstrong number checker in C++

---
+++ Basics/armstrong.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, original, digit, sum = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    original = num;
+
+    while (num > 0) {
+        digit = num % 10;
+        sum = sum + (digit * digit * digit);
+        num = num / 10;
+    }
+
+    if (sum == original) {
+        cout << "Armstrong number";
+    } else {
+        cout << "Not an Armstrong number";
+    }
+
+    return 0;
+}
